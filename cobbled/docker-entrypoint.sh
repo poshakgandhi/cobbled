09029b73329e847243c11f43f04a29caa3cb2986
Poshak <p.gandhi@soton.ac.uk>
2026-06-11 08:20:43 +0100

    Auto-detect SPACE_ID in docker-entrypoint to bind to 7860 on Hugging Face

--- cobbled/docker-entrypoint.sh
+++ cobbled/docker-entrypoint.sh
@@ -1,4 +1,9 @@
-PORT=${PORT:-8000}
+if [ -n "$SPACE_ID" ]; then
+    PORT=${PORT:-7860}
+else
+    PORT=${PORT:-8000}
+fi
+
 uv run manage.py collectstatic --noinput
 uv run manage.py makemigrations
 uv run manage.py migrate --noinput
