0bf2958ddd1bd9f4d403de7390d274170ed26fea
Poshak <p.gandhi@soton.ac.uk>
2026-08-07 15:52:21 +0100

    Group menu sources by project and sort projects/independent sources

--- cobbled/app/main_menu/source.py
+++ cobbled/app/main_menu/source.py
@@ -1,5 +1,5 @@
 from django.db.models import Q
-from iommi import LAST, Field
+from iommi import LAST
 from iommi.main_menu import EXTERNAL, M
 
 from app.forms.source import SourceForm, SourceGaiaInfoForm
@@ -10,11 +10,13 @@ from app.tables.source import SourceTable
 class DynamicSourcesMenu(M):
     def bind(self, request, root):
         import copy
-        from app.models import Source
+        from collections import defaultdict
+
+        from app.models import Project, Source
 
         obj = copy.copy(self)
         base_items = {}
-        for k in ['add', 'view']:
+        for k in ["add", "view"]:
             if k in self.items:
                 base_items[k] = self.items[k]
         obj.items = base_items
@@ -22,26 +24,95 @@ class DynamicSourcesMenu(M):
         user = request.user
         if user.is_authenticated:
             if user.is_staff:
-                sources = Source.objects.all().order_by('name')
+                sources = Source.objects.all().order_by("name")
             elif hasattr(user, "researcher"):
-                sources = Source.objects.filter(
-                    Q(is_valid=True) | Q(created_by=user.researcher)
-                ).distinct().order_by('name')
+                sources = (
+                    Source.objects.filter(Q(is_valid=True) | Q(created_by=user.researcher))
+                    .distinct()
+                    .order_by("name")
+                )
             else:
-                sources = Source.objects.filter(is_valid=True).order_by('name')
+                sources = Source.objects.filter(is_valid=True).order_by("name")
         else:
-            sources = Source.objects.filter(is_valid=True).order_by('name')
+            sources = Source.objects.filter(is_valid=True).order_by("name")
+
+        # Group sources by Project
+        project_sources = defaultdict(list)
+        unassigned_sources = []
+
+        # Pre-fetch all visible projects so we can verify if the user has access
+        all_projects = Project.objects.all()
+        visible_projects = set()
+        for proj in all_projects:
+            if user.has_perm("app.view_project", proj):
+                visible_projects.add(proj)
 
         for source in sources:
-            source_item = M(
+            # Query projects associated with this source
+            source_projects = Project.objects.filter(
+                Q(observation__source=source) | Q(proposal__observation__source=source)
+            ).distinct()
+
+            # Intersection of associated projects and visible projects
+            source_visible_projects = [p for p in source_projects if p in visible_projects]
+
+            if source_visible_projects:
+                for proj in source_visible_projects:
+                    project_sources[proj].append(source)
+            else:
+                unassigned_sources.append(source)
+
+        def make_source_item(source):
+            return M(
                 display_name=source.name,
                 url=source.get_absolute_url(),
                 view=EXTERNAL,
                 icon="minus",
             )
-            source_item.parent = obj
-            source_item._set_name(f"source_{source.pk}")
-            obj.items[source_item.name] = source_item
+
+        def dummy_view(request, **kwargs):
+            from django.http import HttpResponse
+
+            return HttpResponse("")
+
+        # 1. Grouped by Project (sorted by project name)
+        for proj in sorted(project_sources.keys(), key=lambda p: p.name.lower()):
+            proj_sources = project_sources[proj]
+            proj_items = {}
+            for source in sorted(proj_sources, key=lambda s: s.name.lower()):
+                source_item = make_source_item(source)
+                source_item._set_name(f"source_{source.pk}")
+                proj_items[source_item.name] = source_item
+
+            project_menu = M(
+                display_name=proj.name,
+                icon="folder",
+                url=lambda proj_val=proj, **_: proj_val.get_absolute_url(),
+                view=dummy_view,
+                items=proj_items,
+            )
+            project_menu.parent = obj
+            project_menu._set_name(f"project_group_{proj.pk}")
+            obj.items[project_menu.name] = project_menu
+
+        # 2. Independent sources (not linked to any project)
+        if unassigned_sources:
+            other_items = {}
+            for source in sorted(unassigned_sources, key=lambda s: s.name.lower()):
+                source_item = make_source_item(source)
+                source_item._set_name(f"source_{source.pk}")
+                other_items[source_item.name] = source_item
+
+            independent_menu = M(
+                display_name="Independent Sources",
+                icon="folder-open",
+                url=lambda **_: "#",
+                view=dummy_view,
+                items=other_items,
+            )
+            independent_menu.parent = obj
+            independent_menu._set_name("independent_sources")
+            obj.items[independent_menu.name] = independent_menu
 
         return super(DynamicSourcesMenu, obj).bind(request, root)
 
@@ -60,7 +131,8 @@ source_submenu: M = DynamicSourcesMenu(
                     is_valid=dict(
                         # We could exclude this (with `auto__exclude=['is_valid']`) but we don't to show the users.
                         after=LAST,
-                        initial=lambda user, **_: user.is_staff or (user.is_active and hasattr(user, "researcher")),
+                        initial=lambda user, **_: user.is_staff
+                        or (user.is_active and hasattr(user, "researcher")),
                         editable=False,
                     )
                 ),
